7bd01a238b8ea1860a2f8d035284643727588366
whitebbit <66116064+whitebbit@users.noreply.github.com>
2023-11-23 11:09:06 +0300

    create blog app

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("shchool/", include("myapp.urls")),
     path("carshop/", include("carsshop.urls")),
+    path("blog/", include("blog.urls")),
 ]
