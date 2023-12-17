57ab7a9681a35c12e49d003e5487e1e8c0f0ec30
whitebbit <66116064+whitebbit@users.noreply.github.com>
2023-12-17 11:50:54 +0300

    delete all trash

--- blog/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    updated_at = models.DateTimeField(auto_now=True)
--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import blog_detail
-
-urlpatterns = [
-    path("blog/<int:blog_id>/", blog_detail, name="blog_detail"),
-]
